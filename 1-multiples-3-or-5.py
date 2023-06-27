0eaa3d171390a919a50cc7dafc83d49fb0e4b2a8
RameshNeupane <neupaneramesh555@gmail.com>
2023-06-27 22:27:29 +0545

    refactor with function definition

--- 1-multiples-3-or-5.py
+++ 1-multiples-3-or-5.py
@@ -3,5 +3,20 @@ If we list all the natural numbers below 10 that are multiples of 3 or 5, we get
 Find the sum of all the multiples of 3 or 5 below 1000.
 """
 
-a = sum([num for num in range(3, 1000) if num % 3 == 0 or num % 5 == 0])
-print(a)
+def get_sum_of_multiple_of_3_or_5(range_num: int)-> int:
+    """
+    Returns sum of multiples of 3 or 5 below input 'range_num'.
+    
+    Parameters:
+    range_num (int): input number for upper range to calculate get multiples of 3 or 5
+    
+    Returns:
+    (int): output i.e. sum of multiples of 3 or 5
+    """
+    if range_num < 3:
+        return 0
+    else:
+        return sum([num for num in range(3, range_num) if num % 3 == 0 or num % 5 == 0])
+
+result = get_sum_of_multiple_of_3_or_5(1000)
+print(result)
